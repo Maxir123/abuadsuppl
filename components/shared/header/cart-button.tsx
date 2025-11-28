1f178714e9280a84a487c2d96033aa0cbca6888a
Maxir123 <maxiroegbulam@gmail.com>
2025-11-29 00:03:33 +0100

    13-create-cart-sidebar

--- components/shared/header/cart-button.tsx
+++ components/shared/header/cart-button.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import useIsMounted from '@/hooks/use-is-mounted'
 import { cn } from '@/lib/utils'
 import useCartStore from '@/hooks/use-cart-store'
+import useCartSidebar from '@/hooks/use-cart-sidebar'
 
 export default function CartButton() {
   const isMounted = useIsMounted()
@@ -12,6 +13,7 @@ export default function CartButton() {
     cart: { items },
   } = useCartStore()
   const cartItemsCount = items.reduce((a, c) => a + c.quantity, 0)
+  const isCartSidebarOpen = useCartSidebar()
 
   return (
     <Link
@@ -23,18 +25,30 @@ export default function CartButton() {
           <ShoppingCartIcon className='h-5 w-5' />
 
           {isMounted && cartItemsCount > 0 && (
-            <span
-              className={cn(
-                `bg-green-600 text-white rounded-full text-xs font-bold absolute -top-2 -right-2 min-w-[16px] h-4 flex items-center justify-center px-0.5`,
-                cartItemsCount >= 10 && 'text-[10px] min-w-[18px]'
+            <>
+              {/* Badge */}
+              <span
+                className={cn(
+                  `bg-green-600 text-white rounded-full text-xs font-bold absolute -top-2 -right-2 min-w-[16px] h-4 flex items-center justify-center px-0.5`,
+                  cartItemsCount >= 10 && 'text-[10px] min-w-[18px]'
+                )}
+              >
+                {cartItemsCount > 99 ? '99+' : cartItemsCount}
+              </span>
+
+              {/* Sidebar Arrow */}
+              {isCartSidebarOpen && (
+                <div
+                  className='absolute top-[20px] right-[-16px] rotate-[-90deg] z-10 w-0 h-0 
+                  border-l-[7px] border-r-[7px] border-b-[8px] border-transparent 
+                  border-b-background'
+                ></div>
               )}
-            >
-              {cartItemsCount > 99 ? '99+' : cartItemsCount}
-            </span>
+            </>
           )}
         </div>
 
-        {/* Text that appears on hover */}
+        {/* Text on hover */}
         <span className='font-medium text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap'>
           Cart
         </span>
